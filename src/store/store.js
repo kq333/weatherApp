bfb0aa4692ec1d72f60b08d64e0e9b728e4323d0
kq333 <afugowski@yandex.com>
2023-09-18 23:01:38 +0200

    added store to app

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import locationReducer from '../features/locationSlice'
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        location: locationReducer
+    },
 })
