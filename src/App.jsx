099939453ecccb8898e2685161a627da8a334e6a
kq333 <afugowski@yandex.com>
2023-09-20 19:52:31 +0200

    refactoring code, changed how the app works

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDataFromAPI, setInputValue } from './features/locationSlice';
-
 import '../src/App.scss';
 import { Navbar } from './components/navbar/navbar';
 import { ErrorComponent } from './components/error/errorComponent';
@@ -15,19 +13,66 @@ function App() {
   const hasError = useSelector((state) => state.location.hasError);
   const inputValue = useSelector((state) => state.location.inputValue);
 
+  const [cityData, setCityData] = useState([]);
+  const [cityCounter, setCityCounter] = useState({});
+
   useEffect(() => {
-    if (inputValue.length > 0) {
-      dispatch(setInputValue(inputValue));
-      dispatch(fetchDataFromAPI());
-      dispatch(setInputValue(''));
-    }
-  }, [dispatch, inputValue]);
+    const savedDataFromStorage =
+      JSON.parse(localStorage.getItem('cityData')) || [];
+    const savedCounterFromStorage =
+      JSON.parse(localStorage.getItem('cityCounter')) || {};
+
+    setCityData(savedDataFromStorage);
+    setCityCounter(savedCounterFromStorage);
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (inputValue.length > 0) {
+        dispatch(setInputValue(inputValue));
+
+        try {
+          const response = await dispatch(fetchDataFromAPI());
+
+          if (!fetchDataFromAPI.rejected.match(response)) {
+            const newCityData = {
+              city: inputValue.toLowerCase(),
+              date: new Date().toISOString(),
+              temperature: locationData[0]?.current?.feelslike_c,
+            };
+
+            const updatedCityData = [...cityData, newCityData];
+            setCityData(updatedCityData);
+
+            const updatedCityCounter = {
+              ...cityCounter,
+              [newCityData.city]: (cityCounter[newCityData.city] || 0) + 1,
+            };
+            setCityCounter(updatedCityCounter);
+
+            localStorage.setItem('cityData', JSON.stringify(updatedCityData));
+            localStorage.setItem(
+              'cityCounter',
+              JSON.stringify(updatedCityCounter),
+            );
+          }
+        } catch (error) {
+          console.error('Error fetching data from the API:', error);
+        }
+
+        dispatch(setInputValue(''));
+      }
+    };
+
+    fetchData();
+  }, [dispatch, inputValue, locationData, cityData, cityCounter]);
 
   if (isLoading) {
     return <div className='page__loader'>Loading...</div>;
   }
 
   if (hasError) {
+    console.log(hasError);
     return <ErrorComponent />;
   }
 
@@ -36,10 +81,9 @@ function App() {
       <nav className='page__nav'>
         <Navbar />
       </nav>
-
       <main>
         <section>
-          <ListItems locationData={locationData} />
+          <ListItems cityData={cityData} cityCounter={cityCounter} />
         </section>
       </main>
     </div>
