b972f30cb5859b92623b3f00e0f6eb5b227479c5
kq333 <afugowski@yandex.com>
2023-09-20 09:30:04 +0200

    changed div elem to ul li elem and added (s) to search results

--- src/components/listItems/listItems.jsx
+++ src/components/listItems/listItems.jsx
@@ -11,20 +11,20 @@ export const ListItems = ({ locationData }) => {
 
   return (
     <div className='list'>
-      <div className='list__wrapper'>
+      <ul className='list__wrapper'>
         {locationData.map((elem, index) => {
           const cityName = elem.location.name.toLowerCase();
           const count = cityCounts[cityName] || 1;
-          const timesLabel = count >= 2 ? 'times' : 'time';
+          const timesLabel = count >= 2 ? 'times(s)' : 'time(s)';
 
           return (
-            <div className='list__item' key={index}>
+            <li className='list__item' key={index}>
               {elem.location.name} | {elem.location.localtime.slice(0, 10)} |{' '}
               {elem.current.feelslike_c} (st C) | searched: {count} {timesLabel}
-            </div>
+            </li>
           );
         })}
-      </div>
+      </ul>
     </div>
   );
 };
