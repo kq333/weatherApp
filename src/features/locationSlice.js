a5498d00fb7902e118c79d21f175d1ac86fbf020
kq333 <afugowski@yandex.com>
2023-09-21 14:57:30 +0200

    refactor slice, added logic from app.jsx to slice

--- src/features/locationSlice.js
+++ src/features/locationSlice.js
@@ -5,25 +5,42 @@ const initialState = {
     inputValue: "",
     isLoading: false,
     hasError: false,
+    searchError: false,
+    cityData: JSON.parse(localStorage.getItem('cityData')) || [],
+    cityCounter: JSON.parse(localStorage.getItem('cityCounter')) || {},
 };
 
 export const fetchDataFromAPI = createAsyncThunk(
     "fetchData",
-    async (inputValue, { getState }) => {
-
+    async (inputValue, { getState, rejectWithValue, dispatch }) => {
         const state = getState();
         const currentInputValue = state.location.inputValue;
 
-        const URL = `https://api.weatherapi.com/v1/current.json?key=f51e05b2b15d42db90471810231909&q=${currentInputValue}&aqi=no`
+        const URL = `https://api.weatherapi.com/v1/current.json?key=f51e05b2b15d42db90471810231909&q=${currentInputValue}&aqi=no`;
 
-        const response = await fetch(URL);
+        try {
+            const response = await fetch(URL);
 
-        if (!response.ok) {
-            throw new Error("Failed to fetch data");
+            if (!response.ok) {
+                if (response.status === 400) {
+                    dispatch(setSearchError(true));
+                    dispatch(setHasError(false));
+                } else {
+                    dispatch(setSearchError(false));
+                    dispatch(setHasError(true));
+                }
+
+                return rejectWithValue(response);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+
+            dispatch(setSearchError(false));
+            dispatch(setHasError(true));
+            return rejectWithValue(error);
         }
-
-        const data = await response.json();
-        return data;
     }
 );
 
@@ -33,6 +50,12 @@ const locationSlice = createSlice({
     reducers: {
         setInputValue: (state, action) => {
             state.inputValue = action.payload;
+        },
+        setSearchError: (state, action) => {
+            state.searchError = action.payload;
+        },
+        setHasError: (state, action) => {
+            state.hasError = action.payload;
         },
     },
     extraReducers: (builder) => {
@@ -45,14 +68,26 @@ const locationSlice = createSlice({
                 state.isLoading = false;
                 state.hasError = false;
                 state.locationData.push(action.payload);
+
+                const newCityData = {
+                    city: state.inputValue.toLowerCase(),
+                    date: new Date().toISOString(),
+                    temperature: action.payload.current.feelslike_c,
+                };
+                state.cityData.push(newCityData);
+
+                if (!state.cityCounter[newCityData.city]) {
+                    state.cityCounter[newCityData.city] = 1;
+                } else {
+                    state.cityCounter[newCityData.city]++;
+                }
             })
             .addCase(fetchDataFromAPI.rejected, (state) => {
                 state.isLoading = false;
-                state.hasError = true;
             });
     },
 });
 
-export const { setInputValue } = locationSlice.actions;
+export const { setInputValue, setSearchError, setHasError } = locationSlice.actions;
 
 export default locationSlice.reducer;
