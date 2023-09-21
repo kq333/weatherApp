b0fd7d3de8d289d541bb50fea994c9f5ef1f6d7e
kq333 <afugowski@yandex.com>
2023-09-21 14:55:53 +0200

    created loader component

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,9 @@
+import './loader.scss';
+
+export const LoaderComponent = () => {
+  return (
+    <div className='Loader' data-cy='loader'>
+      <div className='Loader__content' />
+    </div>
+  );
+};
